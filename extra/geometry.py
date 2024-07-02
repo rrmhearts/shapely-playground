b8c677d25a27d631fd4004ec5783e865f2046fcf
RYAN R MCCOPPIN <ryan.mccoppin.1.ctr@us.af.mil>
2024-07-02 14:20:02 -0400

    updated functions.

--- extra/geometry.py
+++ extra/geometry.py
@@ -1,29 +1,47 @@
+from __future__ import annotations
 import math
+from math import sqrt
+from numbers import Number
+from typing import List
+import numpy as np
 
 EPS = 1E-9;
 
-# should be replaced, used for circle code only
 class Point(object):
     '''Creates a point on a coordinate plane with values x and y.'''
 
-    def __init__(self, x, y):
+    def __init__(self, x, y=0, z=0):
         '''Defines x and y variables'''
         self.X = x
         self.Y = y
+        self.Z = z
 
     def __add__(self, other):
-        return Point(self.X+other.X, self.Y+other.Y)
+        return Point(self.X+other.X, self.Y+other.Y, self.Z+other.Z)
 
     def __sub__(self, other):
-        return Point(self.X-other.X, self.Y-other.Y)
+        return Point(self.X-other.X, self.Y-other.Y, self.Z-other.Z)
 
     def __str__(self):
-        return f"Point({self.X}, {self.Y})"
+        return f"Point({self.X}, {self.Y}, {self.Z})"
 
     def distance(self, other):
         dx = self.X - other.X
         dy = self.Y - other.Y
         return math.sqrt(dx**2 + dy**2)
+
+
+    def calc_tangents (self, r1, r2) -> Line:
+        r = r2-r1
+        z = self.X*self.X + self.Y*self.Y
+        d = z - r*r
+        if d < -EPS:
+            return 0
+        d = sqrt(abs(d))
+        l = Line( a = ((self.X * r + self.Y * d) / z), \
+                b = ((self.Y * r - self.X * d) / z), \
+                c = r1)
+        return l
 
     @staticmethod
     def testPoint(x=0,y=0):
@@ -40,11 +58,138 @@ class Point(object):
 class Circle(Point):
     '''Creates a circle on a coordinate plane with values x and y.'''
 
-    def __init__(self, x, y, r):
+    def __init__(self, x, y=0, z=0, r=1):
         '''Defines x, y, and radius variables'''
-        self.X = x
-        self.Y = y
+        super().__init__(x, y, z)
         self.radius = r
+    
+    '''
+        tangent_circle: finds points on circle where a tangent line through (Px,Py) touches the circle
+        https://math.stackexchange.com/questions/543496/how-to-find-the-equation-of-a-line-tangent-to-a-circle-that-passes-through-a-g
+    '''
+    def tangent_circle(self, Px: Number, Py: Number, **kwargs):
+                    #center_x: Number=0, center_y: Number=0, radius: Number=1):
+        center_x = self.X
+        center_y = self.Y
+        radius = self.radius
+        if 'x' in kwargs.keys():
+            center_x = kwargs['x']
+        if 'y' in kwargs.keys():
+            center_y = kwargs['y']
+        if 'radius' in kwargs.keys():
+            radius = kwargs['radius']
+
+        dx, dy = Px-center_x, Py-center_y
+        dxr, dyr = -dy, dx
+        d = sqrt(dx**2+dy**2)
+        if d >= radius :
+            rho = radius/d
+            ad = rho**2
+            bd = rho*sqrt(1-rho**2)
+            T1x = center_x + ad*dx + bd*dxr
+            T1y = center_y + ad*dy + bd*dyr
+            T2x = center_x + ad*dx - bd*dxr
+            T2y = center_y + ad*dy - bd*dyr
+            if (d/radius-1) < 1E-8:
+                print('P is on the circumference')
+            return T1x, T1y, T2x, T2y
+
+        else:
+            print('''\
+        Point P: (%g,%g) is inside the circle with centre C: (%g,%g) and radius r=%g.
+        No tangent is possible...''' % (Px, Py, center_x, center_y, radius))
+            return None
+    
+    '''
+        number_tangents_circle: finds number of common tangents between two circles
+    '''
+    def number_tangents_circle(self, b: Circle):
+        # sq root may reduce precision
+        distSq = (self.X - b.X)**2 + (self.Y - b.Y)**2 
+        radSumSq = (self.radius + b.radius)**2
+        if (distSq == radSumSq):  # circles touching
+            return 3              # tangent lines
+        elif (distSq > radSumSq): # circles seperated
+            return 4 # tangent lines
+        else:        # circles overlap
+            return 2 # tangent lines
+    '''
+        tangents: finds all tangent lines between two circles.
+        # https://cp-algorithms.com/geometry/tangents-to-two-circles.html
+    '''
+    def tangents(self, b: Circle):
+        ans: List[Line] = []
+
+        for i in range(-1, 2, 2):
+            for j in range(-1, 2, 2):
+                line = (b-self).calc_tangents(self.radius*i, b.radius*j)
+                if type(line) is Line:
+                    ans.append(line)
+        for i, tan in enumerate(ans):
+            ans[i].c -= tan.a * self.X + tan.b * self.Y
+            ans[i].c - b.radius
+        return ans
+
+
+    '''
+        points_on_tangent: calculates all intersection points of all tangent lines on two circles
+    '''
+    def points_on_tangent(self, b: Circle):
+        answers = self.tangents(b)
+        # f"{self.a}*x + {self.b}*y + {self.c} = 0"
+        points = []
+        for line in answers:
+            x1, x2 = 0, 1
+            y1 = -(line.a*x1 + line.c)/line.b
+            y2 = -(line.a*x2 + line.c)/line.b
+            # dydx = - line.a / line.b
+            x3, y3 = self.X, self.Y
+            k = ((y2-y1) * (x3-x1) - (x2-x1) * (y3-y1)) / ((y2-y1)**2 + (x2-x1)**2)
+            x4 = x3 - k * (y2-y1)
+            y4 = y3 + k * (x2-x1)
+            points.append((x4,y4))
+
+            # second circle
+            x3, y3 = b.X, b.Y
+            k = ((y2-y1) * (x3-x1) - (x2-x1) * (y3-y1)) / ((y2-y1)**2 + (x2-x1)**2)
+            x4 = x3 - k * (y2-y1)
+            y4 = y3 + k * (x2-x1)
+            points.append((x4,y4))
+            # dydx = (x1-a)/(y1-b)
+            # x1^2 + y1^2 = r^2
+            # dydx * y1- b*dydx = x1 -a
+            # y1 = (x1 - a)/dydx + b
+        return points
+    
+class Sphere(Circle):
+    '''Creates a circle on a coordinate plane with values x and y.'''
+
+    def __init__(self, x, y=0, z=0, r=1):
+        '''Defines x, y, and radius variables'''
+        super().__init__(x, y, r)
+        self.Z = z
+
+    def __iter__(self):
+        return iter((self.X, self.Y, self.Z, self.radius))
+
+    def data_for_sphere(self):
+        center_x, center_y, center_z, radius = self
+
+        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi/2:10j]
+        x = radius*np.cos(u)*np.sin(v) + center_x
+        y = radius*np.sin(u)*np.sin(v) + center_y
+        z = radius*np.cos(v) + center_z
+        return x, y, z
+    
+    # If ALL points are inside sphere, return True
+    def inside_sphere(self, ps: list[Point]) -> bool:
+        cx, cy, cz, r = self.X, self.Y, self.Z, self.radius
+
+        for p in ps:
+            x, y, z = p.X, p.Y, p.Z
+            if (x - cx)**2 + (y - cy)**2 + (z - cz)**2 > r**2:
+                return False # outside sphere
+        return True
 
 class Line(object):
 
@@ -58,7 +203,67 @@ class Line(object):
     
     def __repr__(self) -> str:
         return f"{self.a}*x + {self.b}*y + {self.c} = 0"
-    
+
+class Cylinder(Sphere):
+    def __init__(self, x, y, z, r, h):
+        '''Defines x, y, and radius variables'''
+        super().__init__(x, y, z, r)
+        self.height = h
+
+    def __iter__(self):
+        return iter((self.X, self.Y, self.Z, self.radius, self.height))
+
+    def data_for_cylinder_along_z(self, **kwargs):
+        center_x, center_y, center_z, radius, height_z = self
+        z = np.linspace(center_z, height_z, 50)
+        theta = np.linspace(0, 2*np.pi, 50)
+        theta_grid, z_grid=np.meshgrid(theta, z)
+        x_grid = radius*np.cos(theta_grid) + center_x
+        y_grid = radius*np.sin(theta_grid) + center_y
+        return x_grid,y_grid,z_grid
+
 if __name__ == "__main__":
 
     print (f"distance = {Point.testPoint()}" )
+
+    center_x, center_y = 0, 0
+    radius = 5
+    Px, Py = 10, 1
+    # circle = Circle(center_x, center_y, radius)
+    # point 1, point 2
+    # T1x, T1y, T2x, T2y = tangent_circle(Px, Py, center_x=center_x, center_y=center_y, radius=radius)
+    T1x, T1y, T2x, T2y = Circle(center_x, center_y, radius).tangent_circle(Px, Py)
+    # T1x, T1y, T2x, T2y = tangent_circle(Px, Py, circle)
+
+    print(f'The tangent of the circle at ({center_x}, {center_y}) of radius {radius}')
+    print(f'that passes through ({Px, Py}) has')
+    print('the tangent points:')
+    print('\tT1: (%g,%g),  T2: (%g,%g).'%(T1x, T1y, T2x, T2y))
+
+    print('The equations of the lines P-T1 and P-T2:')
+    print('\t%+g·y%+g·x%+g = 0'%(T1x-Px, Py-T1y, T1y*Px-T1x*Py))
+    print('\t%+g·y%+g·x%+g = 0'%(T2x-Px, Py-T2y, T2y*Px-T2x*Py))
+
+    # Number of tangent lines
+
+    c1 = Circle(-10, 8, 30)
+    c2 = Circle(14, -24, 10)
+    t = c1.number_tangents_circle(c2);
+
+    if (t == 3):
+        print("There are 3 common tangents between the circles.")
+    elif (t == 4):
+        print("There are 4 common tangents between the circles.")
+    else:
+        print("There are 2 common tangents between the circles.")
+ 
+    # Actually get tangents
+    answers = [str(tan) for tan in Circle(10, 1, 5).tangents(Circle(0,0,3))]
+    print("Num tangents: \n", "\n".join( answers ) )
+
+    print("Points? ", Circle(10, 1, 5).points_on_tangent(Circle(0,0,3)))
+
+    s = Sphere(0, 0, 0, 1)
+    print("Inside sphere:", s.inside_sphere([Point(0.5,0.5,0.5)]) )
+    print("Outside sphere [False]:", s.inside_sphere([Point(0.9, 1.5, 1)]) )
+
--- playground.py
+++ playground.py
@@ -1,24 +1,34 @@
-from shapely.geometry import Point, Polygon, LineString, mapping
-import matplotlib.pyplot as plt
-import json
-import shapely
 import itertools
+import json
+from math import sqrt
+from typing import Union, overload
+
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import shapely
+from shapely import polygonize, wkt
+import shapely.affinity
+from shapely.geometry import LineString, Point, Polygon, mapping
+import shapely.plotting
 
-from shapely import wkt, polygonize
-from typing import Union
-from math import sqrt
+from extra.geometry import Sphere
 
-def polygon_to_tuple_points(polygon: Polygon):
+
+def polygon_to_tuple_points(polygon: Polygon, zcoords=() ):
     poly_coordinates = mapping(polygon)['coordinates'][0]
-    # poly_ = [{'lat': coords[1],'lon': coords[0]} for coords in poly_coordinates]
-    poly_ = [(coords[0], coords[1]) for coords in poly_coordinates]
+    try:
+        poly_ = [(coords[0], coords[1], coords[2], *zcoords) for coords in poly_coordinates]
+    except IndexError:
+        # poly_ = [{'lat': coords[1],'lon': coords[0]} for coords in poly_coordinates]
+        poly_ = [(coords[0], coords[1], *zcoords) for coords in poly_coordinates]
     # Mind that the above output will contain one point two times, the first and last points
     return poly_[:-1]
 
-def point_to_tuple(point: Point):
-    return tuple(point.coords)[0]
+def point_to_tuple(point: Point|tuple):
+    if isinstance(point, Point):
+        return tuple(point.coords)[0]
+    return point
 
 def points_to_intersections(points: list, rounder:int = 1, criterion=None):
     inters = []
@@ -55,6 +65,62 @@ def edges_to_polygon(edges: Union[tuple, list]):
     # return first polygon in collection
     return shapely.get_geometry(polyy2, 0) 
 
+def polygon2D(obs: Polygon):
+    return Polygon([el[0:2] for el in polygon_to_tuple_points(obs)])
+
+def point2D(p: Point):
+    return shapely.transform(p, lambda x: x) 
+
+
+def distance_between(p1, p2):
+    np1 = np.array(point_to_tuple(p1))
+    np2 = np.array(point_to_tuple(p2))
+    return np.sqrt(np.sum((np1-np2)**2))
+
+def middle_points( ps: list[Point]):
+    middles = set()
+    for p, q in itertools.combinations(ps, 2):
+        middles.add( Point( (p.x+q.x)/2, (p.y+q.y)/2, (p.z+q.z)/2 if p.has_z and q.has_z else 0 ))
+    ps.extend( list(middles) )
+
+def inside_ellipse( ps: list[Point], sam: Polygon):
+    # sam.bounds contains the width and height of the sam if desired to model ellipse
+    minx, miny, maxx, maxy = sam.bounds
+    radx, rady = (maxx - minx)/2, (maxy - miny)/2
+    center = shapely.centroid(sam)
+    # height = shapely.hausdorff_distance(sam, center)
+    height = max(radx, rady)
+    cx, cy, cz = center.x, center.y, center.z if center.has_z else 0
+
+    middle_points(ps) # extends ps
+    for p in ps:
+        x, y = p.x, p.y
+        z = p.z if p.has_z else 0
+        if (x - cx)**2/radx**2 \
+            + (y - cy)**2/rady**2 + (z - cz)**2/height**2 < 1:
+            return True
+    return False
+
+@overload
+def inside_sphere(ps: list[Point], s: Sphere) -> bool: ...
+@overload
+def inside_sphere(ps: list[Point], center: Point, radius: float) -> bool: ...
+def inside_sphere(*args) -> bool:
+    if len(args) == 3:
+        ps, center, radius = args
+        cx, cy, cz, r = center.x, center.y, center.z if center.has_z else 0, radius
+    else:
+        ps, s = args
+        cx, cy, cz, r = s.X, s.Y, 0, s.radius
+
+    middle_points(ps)
+    for p in ps:
+        x, y = p.x, p.y
+        z = p.z if p.has_z else 0
+        if (x - cx)**2 + (y - cy)**2 + (z - cz)**2 < r**2:
+            return True
+    return False
+
 def to_star(polygon):
     minx, miny, maxx, maxy = polygon.bounds
     # get the centroid
@@ -88,14 +154,27 @@ def to_pentagon(polygon, large: bool=True):
     pentagon = pentagon.simplify(tolerance=0.01) 
     # for simple whole number polygons, i%8 == 1 is large, and 0 is smaller,
     # but with simplify, it is random, so
-    pZero = Polygon([ p for i, p in enumerate(pentagon.exterior.coords[:-1]) if i%8==0 ])
-    pOne = Polygon([ p for i, p in enumerate(pentagon.exterior.coords[:-1]) if i%8==1 ])
-
-    oneLarger = pOne.area > pZero.area
-    if oneLarger and large or not oneLarger and not large:
-        return pOne
+    if len(pentagon.exterior.coords[:-1]) > 8:
+        pZero = Polygon([ p for i, p in enumerate(pentagon.exterior.coords[:-1]) if i%8==0 ])
+        pOne = Polygon([ p for i, p in enumerate(pentagon.exterior.coords[:-1]) if i%8==1 ])
+        oneLarger = pOne.area > pZero.area
+        if oneLarger and large or not oneLarger and not large:
+            return pOne
+    else:
+        # There are not enough points for the above math to work, just return exterior
+        pZero = Polygon([p for p in pentagon.exterior.coords[:-1]])
     return pZero
 
+def verify2DPolygonVisible(pin: Polygon, pout: Polygon|list) -> bool:
+    pin = polygon2D(pin)
+    if isinstance(pout, Polygon):
+        poly = polygon2D(pout)
+        return not poly.contains(pin)
+    for poly in pout:
+        poly = polygon2D(poly)
+        if poly.contains(pin):
+            return False
+    return True
 
 def complete_graph_from_list(L, create_using=None):
     G = nx.empty_graph(L,create_using)
@@ -106,6 +185,41 @@ def complete_graph_from_list(L, create_using=None):
             edges = itertools.combinations(L,2)
         G.add_edges_from(edges)
     return G
+
+def path_to_edges(path):
+    edges = []
+    for i, el in enumerate(path):
+        edges.append((el, path[(i + 1) % len(path)]))
+    return edges[:-1]
+
+def remove_lines_through_polygons(lines, polygons):
+  """
+  Removes lines that intersect with any of the given polygons.
+
+  Args:
+    lines: A list of Shapely LineString objects.
+    polygons: A list of Shapely Polygon objects.
+
+  Returns:
+    A list of Shapely LineString objects that do not intersect with any of the
+    given polygons.
+  """
+
+  # Create a set of all the points that are contained within any of the polygons.
+  polygon_points = set()
+  for polygon in polygons:
+    polygon_points.update(polygon.exterior.coords)
+    for hole in polygon.interiors:
+      polygon_points.update(hole.coords)
+
+  # Filter the lines to only include those that do not intersect with any of the
+  # polygon points.
+  filtered_lines = []
+  for line in lines:
+    if not any(line.intersects(Point(point)) for point in polygon_points):
+      filtered_lines.append(line)
+
+  return filtered_lines
 
 if __name__ == '__main__':
     poly = Polygon([[14.471329,46.037286],[14.467378,46.036733],[14.468441,46.034822]])
@@ -138,6 +252,7 @@ if __name__ == '__main__':
     )
 
     p = Point(2, 2.5)
+    p = Point(p.x, 5)
     plt.scatter(p.x, p.y)
     # plt.show()
     poly1 = wkt.loads("POLYGON((1 1,2 1,3 3,2 4,1 3,1 1))")
@@ -155,3 +270,16 @@ if __name__ == '__main__':
     plt.plot(*shared_region.exterior.xy)
     plt.show()
 
+    S = complete_graph_from_list(["a", "b", "c", "d"])
+    print (S.edges() )
+    S = complete_graph_from_list([(1,2), (3,4), (5,6)])
+    print (S.edges() )
+
+
+    lstPolys = [shapely.minimum_bounding_circle(Polygon([(1,2), (3,4), (-20, 2), (30, -1)]) ), ]
+    square = list( map(to_pentagon, lstPolys) )
+
+    # square[0] = Polygon([ p for i, p in enumerate(polygon_to_tuple_points(square[0])) if i%8==0 ])
+    # shapely.plotting.plot_polygon(lstPolys[0])
+    # shapely.plotting.plot_polygon(square[0])
+    # plt.show()
